1485b4c96ad5c60fe52665fe802763c923daf8b5
Will Koste <willkoste@gmail.com>
2020-03-15 09:54:28 -0400

    Profile Thing Fixed

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react';
 import Moment from 'react-moment';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import {deleteEducation, deleteExperience} from '../../actions/profile';
+import {deleteEducation} from '../../actions/profile';
 
 const Education = ({education, deleteEducation}) => {
   const educations = education.map(edu => (
