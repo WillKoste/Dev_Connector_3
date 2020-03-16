484be72d210b2b4ff5930ed984910846afe0b985
Will Koste <willkoste@gmail.com>
2020-03-16 09:45:30 -0400

    Create Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from './types';
 
 export const getPosts = () => async dispatch => {
@@ -60,7 +61,7 @@ export const removeLike = id => async dispatch => {
 
 export const deletePost = id => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -68,6 +69,31 @@ export const deletePost = id => async dispatch => {
     });
 
     dispatch(setAlert('Post Removed!', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status}
+    });
+  }
+}
+
+
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    } 
+  }
+  
+  try {
+    const res = await axios.post(`/api/posts`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created!', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
